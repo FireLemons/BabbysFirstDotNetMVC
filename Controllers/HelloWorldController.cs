c1572391b0d819f42826f64b2bd52fadf2de1445
FireLemons <shenyang64@gmail.com>
2017-12-10 23:23:30 -0600

    created a view

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -9,10 +9,9 @@ namespace Exploration4.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/
-        public /*ActionResult*/ string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
-            //return View();
+            return View();
         }
 
         // GET: /HelloWorld/Welcome/
